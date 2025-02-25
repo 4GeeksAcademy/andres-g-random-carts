4424ad8bdce98ec5477a002b59f0042a3786c8d6
Andrés Guaipo <andresguaipo.16@gmail.com>
2025-02-25 22:34:18 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let icons = ["suit-diamonds", "suit-heart", "suit-spades", "suit-clubs"]
+let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+let randomIconsNumber = Math.floor(Math.random() * icons.length)
+let randomCardNumber = Math.floor(Math.random() * cardNumber.length)
+
+document.getElementById('cardContent').innerHTML = cardNumber[randomCardNumber];
+document.getElementById('theCard').classList.add(icons[randomIconsNumber])
 };
